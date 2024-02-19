932a2df956ab781182f1f58dafe2ceab1e105897
OlesiaZaozirna <137696365+OlesiaZaozirna@users.noreply.github.com>
2024-02-19 23:05:51 +0200

    update

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -5,7 +5,7 @@ import { useSelector } from 'react-redux';
 import NavbarMenu from './NavbarMenu/NavbarMenu';
 import NavbarAuth from 'components/Navigation/NavbarAuth/NavbarAuth';
 import NavbarUser from './NavbarUser/NavbarUser';
-
+import { MdPermPhoneMsg } from "react-icons/md";
 import { selectIsLogin } from '../../redux/auth/auth-selectors';
 import css from "./Navigation.module.css"
 
@@ -16,6 +16,7 @@ const Navigation = () => {
   return (
     <nav className={css.navbar}>
       <Link to="/" className={css.logo}>
+        <MdPermPhoneMsg  className={css.logo_icon} />
         Your Phonebook
       </Link>
       <NavbarMenu />
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,29 @@
 import css from "./HomePage.module.css"
+import { useEffect, useState } from 'react';
 
 const HomePage = () => {
-    return (
-        <div>
-            <h1>Welcome to the Phonebook !</h1>
+      const [opacity, setOpacity] = useState(0);
+
+   useEffect(() => {
+        let start;
+        const animate = (timestamp) => {
+            if (!start) start = timestamp;
+            const progress = timestamp - start;
+            if (progress < 1000) {            
+                setOpacity(progress / 1000);           
+                requestAnimationFrame(animate);
+            } else {
+                setOpacity(1); 
+            }
+        };
+        requestAnimationFrame(animate);
+    }, []);
+
+     return (
+        <div className={css.container}>
+            <h1 className={css.title} style={{ opacity }}>Welcome to the Phonebook !</h1>
         </div>
     )
 }
-export default HomePage
+
+export default HomePage;
